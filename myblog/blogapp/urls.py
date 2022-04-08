4a7cd29de5f71c34ac1cf4b656d597c1ff3eabbb
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-04-08 20:28:47 +0300

    New features: paginator, CRUD posts.

--- myblog/blogapp/urls.py
+++ myblog/blogapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'blogapp'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
+    path('user/<str:username>/', views.UserPostListView.as_view(), name='userpost'),
 ]
